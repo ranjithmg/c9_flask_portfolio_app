e3cfbfc15b2402af546fa908b54646d41d4011f3
ranjithmg <40706518+ranjithmg@users.noreply.github.com>
2018-06-30 16:01:55 +1000

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new name = str(name) +"Like to play football"
-	return render_template('index.html', name=name)
+	new_name = str(name) +"Like to play football"
+	return render_template('index.html', name=new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
